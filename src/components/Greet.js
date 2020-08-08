ef0704b2e165d34dfb64ccde9e62bac94abdcfcd
MatMatzZ <MatMatzZ@users.noreply.github.com>
2020-08-08 12:06:53 +0800

    first commit

---
+++ src/components/Greet.js
@@ -0,0 +1,7 @@
+import React from 'react'
+// Stateless Functional Component
+
+const Greet = ()=> <h1>Hello MatMat!</h1>
+
+// export const Greet = () => <h1>Hello MatMat!</h1>
+export default Greet;
