80d9f15f3fe5f8f363e029654e25ef1013b59adb
MatMatzZ <MatMatzZ@users.noreply.github.com>
2020-08-08 14:46:00 +0800

    Updates

---
+++ precache-manifest.46c02627298092c6813c6f233d7fd5b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a28b501daff0645fcde49522164fcc1",
+    "url": "/React-Repository/index.html"
+  },
+  {
+    "revision": "41a67b36d6a4fe1718b8",
+    "url": "/React-Repository/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "001f0fb8c375fd8a6d43",
+    "url": "/React-Repository/static/js/2.86c41a82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Repository/static/js/2.86c41a82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41a67b36d6a4fe1718b8",
+    "url": "/React-Repository/static/js/main.26c137ec.chunk.js"
+  },
+  {
+    "revision": "19c36dafeb5198e88abf",
+    "url": "/React-Repository/static/js/runtime-main.7c0ec4e0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Repository/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/components/Greet.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-// Stateless Functional Component
-
-const Greet = ()=> <h1>Hello MatMat!</h1>
-
-// export const Greet = () => <h1>Hello MatMat!</h1>
-export default Greet;
