7f8e32d90bea5fc52c8f3a2a605fb5d7d2072a81
MatMatzZ <MatMatzZ@users.noreply.github.com>
2020-08-10 14:30:01 +0800

    Props tutorial

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Welcome from './components/Welcome'
 function App() {
   return (
     <div className="App">
+      <h1>Functional Statement</h1>
+      <hr></hr>
       <Greet name ="Bruce" heroName="Batman">
         <p>I am dark knight</p>
       </Greet>
@@ -14,6 +16,12 @@ function App() {
         <button>Click me</button>
       </Greet>
       <Greet name ="Diana" heroName= "Wonderwoman"/>
+      <hr></hr>
+      <h1>Class Statement</h1>
+      <hr></hr>
+      <Welcome name ="Bruce" heroName="Batman"/>
+      <Welcome name ="Clark" heroName= "Superman"/>
+      <Welcome name ="Diana" heroName= "Wonderwoman"/>
     </div>
   );
 }
