631e92fce5039a60f1cf64493e97496f3acb88a4
MatMatzZ <MatMatzZ@users.noreply.github.com>
2020-08-10 14:53:54 +0800

    State tutorial

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet'
 import Welcome from './components/Welcome'
+import Message from './components/Message'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Functional Statement</h1>
-      <hr></hr>
-      <Greet name ="Bruce" heroName="Batman">
-        <p>I am dark knight</p>
-      </Greet>
-      <Greet name ="Clark" heroName= "Superman">
-        <button>Click me</button>
-      </Greet>
-      <Greet name ="Diana" heroName= "Wonderwoman"/>
-      <hr></hr>
-      <h1>Class Statement</h1>
-      <hr></hr>
-      <Welcome name ="Bruce" heroName="Batman"/>
-      <Welcome name ="Clark" heroName= "Superman"/>
-      <Welcome name ="Diana" heroName= "Wonderwoman"/>
-    </div>
-  );
-}
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <Message/>
+  
+        {/* <h1>Functional Statement</h1>
+        <hr></hr>
+        <Greet name ="Bruce" heroName="Batman">
+          <p>I am dark knight</p>
+        </Greet>
+        <Greet name ="Clark" heroName= "Superman">
+          <button>Click me</button>
+        </Greet>
+        <Greet name ="Diana" heroName= "Wonderwoman"/>
+        <hr></hr>
+        <h1>Class Statement</h1>
+        <hr></hr>
+        <Welcome name ="Bruce" heroName="Batman"/>
+        <Welcome name ="Clark" heroName= "Superman"/>
+        <Welcome name ="Diana" heroName= "Wonderwoman"/> */}
+      </div>
+    );
+  }
+  }
+  
 
 export default App;
